f3941450182e0503001298dbfb37770893ad2fe8
rojoescala <rojo.escala@gmail.com>
2024-09-24 01:43:32 -0700

    Changes for gh deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import ErrorPage from './ErrorPage.tsx';
 import ProjectHome from './pages/ProjectHome.tsx';
 import Navbar from './components/Navbar.tsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Navbar />,
