d7e8449621065ff9f67ffb6fb5b7fb418a494584
rojoescala <rojo.escala@gmail.com>
2024-09-24 18:02:05 -0700

    change to base vite config for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: './', // Ajusta esto según el nombre de tu repositorio
+   base: './', // for github pages
 })
