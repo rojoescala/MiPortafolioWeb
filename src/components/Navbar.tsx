c67f8dfe967a8dec6273af928e9713bcb6efbafb
rojoescala <rojo.escala@gmail.com>
2024-09-28 14:02:47 -0700

    Logo and Navbar adjustments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from "react-router-dom";
 import { useState } from "react";
+import logo from '../assets/LogoRJ.png'
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -10,26 +11,26 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="w-full fixed bg-black z-50 border-b-2 border-yellow-900 sm:border-b-4">
+            <nav className="w-full fixed bg-black z-50 border-b-4 border-yellow-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <Link to={`/`} className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Roberto Escalante</span>
+                    <Link to={`/`} className="flex items-center space-x-6 rtl:space-x-reverse">
+                        <img src={logo} className="h-10" alt="logo" />
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">My Portfolio</span>
                     </Link>
                     <button
                         onClick={toggleMenu}
                         type="button"
                         className="flex items-center p-2 w-8 h-8 justify-center text-sm rounded-lg text-white hover:bg-yellow-800 md:hidden"
                         aria-controls="navbar-default"
-                        aria-expanded={isOpen}
-                    >
+                        aria-expanded={isOpen}>
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
                     </button>
-                    <div className={`w-full md:block md:w-auto ${isOpen ? "block" : "hidden"}`} id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x md:mt-0 md:border-0">
-                            <li>
+                    <div className={`w-full md:block md:w-auto ${isOpen ? "block border-t-4 mt-4 border-yellow-900" : "hidden"}`} id="navbar-default">
+                        <ul className="font-medium flex flex-col md:flex-row p-4 md:p-0 mt-4 rounded-lg md:space-x-8 rtl:space-x md:mt-0 md:border-0 md:justify-center w-full">
+                            <li className="flex justify-center w-full">
                                 <Link to={`/`} className="flex items-center py-2 px-2 bg-transparent border-0 text-white hover:text-yellow-800">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-fill mx-2" viewBox="0 0 16 16">
                                         <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
@@ -38,7 +39,7 @@ const Navbar = () => {
                                     Home
                                 </Link>
                             </li>
-                            <li>
+                            <li className="flex justify-center w-full">
                                 <a href="#" className="flex items-center py-2 px-2 bg-transparent border-0 text-white hover:text-yellow-800">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-plus-fill mx-2" viewBox="0 0 16 16">
                                         <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
